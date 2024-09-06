ad01bf85343c676c1d1c1185c310b4aab5050bc1
Rudik-96 <rudik.mamyan96@gmail.com>
2024-09-06 13:13:02 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
             <Header/>
             <Routes>
                 <Route index element={<ProductList/>}/>
-                <Route index path={'form'} element={<Form/>}/>
+                <Route index path={'/form'} element={<Form/>}/>
             </Routes>
         </div>
     );
